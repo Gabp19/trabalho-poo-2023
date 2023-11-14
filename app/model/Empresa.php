807fd5cf87045d0a1235d99c6f0bf46cfeaa564d
Gabp19 <gabperera19@gmail.com>
2023-11-14 10:39:40 -0300

    atualizar

---
+++ app/model/Empresa.php
@@ -0,0 +1,87 @@
+<?php
+
+class Empresa
+{ 
+    private $nome;
+    private $cnpj;
+    private $endereco;
+    private $telefone;
+
+
+    /* Metodo construtor da classe. e executado ao instanciar um objeto */ 
+    public function __consruct ($nome,$tempo,$valor,$logo)
+    {
+        $this->nome = $nome;
+        $this->cnpj = $cnpj;
+        $this->endereco = $endereco;
+        $this->logo = $logo;
+        
+
+    }
+
+    // nome 
+    public function getNome ()
+    {
+        return $this->nome;
+    }
+
+    
+    public function setNome ($nome)
+    {
+        $this->nome = $nome;
+    }
+    
+    // cnpj
+    public function getCnpj ()
+    {
+        return $this->cnpj;
+    }
+
+    public function setCnpj ($cnpj)
+    {
+         $this->cnpj = $cnpj;
+    }
+
+    //endereço
+    public function getEndereco ()
+    {
+        return $this->endereco;
+    }
+
+    public function setEndereco ($endereco)
+    {
+         $this->endereco = $endereco;
+    }
+
+    //telefone
+    public function getTelefone ()
+    {
+        return $this->telefone;
+    }
+
+    public function setTelefone ($telefone)
+    {
+         $this->telefone = $telefone;
+    }
+
+    //método pra cadastrar uma empresa
+    public function cadastrar ()
+    {
+         
+    }
+
+    //método pra atualizar uma empresa
+    public function atualizar ()
+    {
+         
+    }
+
+    //método pra apagar uma empresa
+    public function apagar ()
+    {
+         
+    }
+}
+
+
+?>
