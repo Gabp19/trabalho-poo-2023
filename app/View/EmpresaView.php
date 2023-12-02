e7ab9364339a72012b2e1b25b8873aad83ec0a58
Gabp19 <gabperera19@gmail.com>
2023-12-01 21:52:53 -0300

    empreview tarefa 22/11

---
+++ app/View/EmpresaView.php
@@ -0,0 +1,31 @@
+<?php
+
+class EmpresaView
+{
+    //Metodo para exibir a lista de empresas
+    public function exibirEmpresa()
+    {
+        echo "<h3> Lista de Empresas </h3> ";
+        echo "<table border='1'>";
+
+        echo "<tr>
+            
+            <th>Nome</th>
+            <th>CNPJ</th>
+            <th>Endereco</th>
+            <th>Telefone</th>
+            </tr>
+    
+    
+          ";
+    }   echo"</tables>";
+
+
+
+
+}
+
+
+
+
+?>
